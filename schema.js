52e0f04947f055629a6ff39dd9ca630c73b08d6c
nikitazapekin <wotblitz362@mail.ru>
2024-02-18 23:44:20 +0300

    first

---
+++ schema.js
@@ -0,0 +1,80 @@
+const {buildSchema} = require('graphql')
+const schema =  buildSchema(`
+type User {
+    id: ID
+    username: String
+    password: String 
+}
+
+input UserInput {
+    id: ID
+    username: String!
+  password: String!
+  
+}
+input PostInput {
+    id: ID
+    title: String!
+    content: String!
+}
+
+type Query {
+    getAllUsers: [User]
+    getUser(id: ID): User
+}
+type Mutation {
+    createUser(input: UserInput): User
+}
+
+
+
+`)
+
+/*
+const schema = buildSchema(`
+    
+    type User {
+        id: ID
+        username: String
+        age: Int
+        posts: [Post]
+    }
+    type Post {
+        id: ID
+        title: String
+        content: String
+    }
+    
+    input UserInput {
+        id: ID
+        username: String!
+        age: Int!
+        posts: [PostInput]
+    }
+    input PostInput {
+        id: ID
+        title: String!
+        content: String!
+    }
+    
+    type Query {
+        getAllUsers: [User]
+        getUser(id: ID): User
+    }
+    type Mutation {
+        createUser(input: UserInput): User
+    }
+
+`)
+*/
+module.exports = schema 
+/*
+mutation {
+  createUser(input: {
+    username: "Gleb"
+   password: "hhackjnbjs"
+  }) {
+    id username
+  }
+}
+*/
